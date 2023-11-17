358c0c6f3894781aef4fabdc597b0ec410d8f251
keny <keny7503@gmail.com>
2023-11-17 08:49:57 +0700

    Add TabBar to SideBar

--- Pokemon-Utility/Views/MainInterface.axaml.cs
+++ Pokemon-Utility/Views/MainInterface.axaml.cs
@@ -4,6 +4,8 @@ using Avalonia.Interactivity;
 using Avalonia.Layout;
 using Avalonia.Media;
 using Pokemon;
+using Pokemon_Utility.Views.Browsing;
+using Pokemon_Utility.Views.TeamBuilder;
 
 namespace Pokemon_Utility.Views;
 
@@ -12,16 +14,22 @@ public partial class MainInterface : Panel
     private int id = 1;
     private TextBlock tb = new TextBlock { Text = "Data"};
     private Button btn = new Button { Content = "Click"};
+    
+    private BrowsingView browsingView = new Browsing.BrowsingView();
+    private TeamView teamView = new TeamBuilder.TeamView();
     public MainInterface()
     {
         InitializeComponent();
         SideBar.Children.Add(tb);
         SideBar.Children.Add(btn);
+        TabBar.Items.Add("Browse");
+        TabBar.Items.Add("Team");
+        TabBar.SelectedIndex = 0;
+        
 
         if (MainContext.Connected)
         {
-            var browsingView = new Browsing.BrowsingView();
-            var teamView = new TeamBuilder.TeamView();
+            
             
             this.Children.Add(browsingView);
             MainContext.Query(
@@ -32,6 +40,7 @@ public partial class MainInterface : Panel
                 onFailure: () => tb.Text = "No Connection"
             );
             btn.Click += OnClick;
+            TabBar.SelectionChanged += OnSelect;
 
         }
         else
@@ -42,6 +51,19 @@ public partial class MainInterface : Panel
 
 
     }
+
+    private void OnSelect(object? sender, SelectionChangedEventArgs e)
+    {
+        if ((sender as ListBox).SelectedIndex == 0)
+        {
+            this.Children[1] = browsingView;
+        }
+        else
+        {
+            this.Children[1] = teamView;
+        }
+    }
+    
 
     private void OnClick(object? sender, RoutedEventArgs e)
     {
