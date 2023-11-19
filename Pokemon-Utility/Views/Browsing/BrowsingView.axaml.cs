c9c2730e25e9dfbf7d32b86556ba561f9866c786
trunghieu41003 <trunghieu41003@gmail.com>
2023-11-19 21:33:14 +0700

    hieu

--- Pokemon-Utility/Views/Browsing/BrowsingView.axaml.cs
+++ Pokemon-Utility/Views/Browsing/BrowsingView.axaml.cs
@@ -116,26 +116,7 @@ public partial class BrowsingView : Panel
             this.PokemonFoundTextblock(nOfPokemon);
             this.PokemonList(nOfPokemon, pokemons);
         };
-        //event when user choose item in comboBox  
-        string GetSelectedItemString(ComboBox comboBox)
-        {
-            string selectedItem = string.Empty;
-
-            // Handle the SelectionChanged event of the ComboBox
-            comboBox.SelectionChanged += (sender, args) =>
-            {
-                if (args.Source is ComboBox cb)
-                {
-                    if (cb.SelectedItem != null)
-                    {
-                        selectedItem = cb.SelectedItem.ToString();
-
-                    }
-                }
-            };
-
-            return selectedItem;
-        }
+        
         searchIcon = new Image();
         searchIcon.Source = new Bitmap(AssetLoader.Open(new Uri("avares://Pokemon-Utility/Assets/search_icon.png")));
         //searchButton.Content = searchButton;
---
+++ Pokemon-Utility/Views/Filter.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    internal class Filter
+{
+        //event when user choose item in comboBox  
+        string GetSelectedItemString(ComboBox comboBox)
+        {
+            string selectedItem = string.Empty;
+
+            // Handle the SelectionChanged event of the ComboBox
+            comboBox.SelectionChanged += (sender, args) =>
+            {
+                if (args.Source is ComboBox cb)
+                {
+                    if (cb.SelectedItem != null)
+                    {
+                        selectedItem = cb.SelectedItem.ToString();
+
+                    }
+                }
+            };
+
+            return selectedItem;
+        }
+    }
+}
