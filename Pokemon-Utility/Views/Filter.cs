42241531bbe4daac8ce9d7b6348287f2e91889c4
trunghieu41003 <trunghieu41003@gmail.com>
2023-11-20 07:41:04 +0700

    hieu

--- Pokemon-Utility/Views/Filter.cs
+++ Pokemon-Utility/Views/Filter.cs
@@ -7,27 +7,40 @@ using System.Threading.Tasks;
 
 namespace Pokemon
 {
-    internal class Filter
-{
-        //event when user choose item in comboBox  
-        string GetSelectedItemString(ComboBox comboBox)
+    public class Filter : ComboBox
+    {
+       public Filter()
         {
-            string selectedItem = string.Empty;
 
-            // Handle the SelectionChanged event of the ComboBox
-            comboBox.SelectionChanged += (sender, args) =>
-            {
-                if (args.Source is ComboBox cb)
-                {
-                    if (cb.SelectedItem != null)
-                    {
-                        selectedItem = cb.SelectedItem.ToString();
-
-                    }
-                }
-            };
-
-            return selectedItem;
         }
+       public Filter(ComboBox filter)
+       {
+
+            //event when user choose item in comboBox  
+            string GetSelectedItemString(ComboBox comboBox)
+            {
+                string selectedItem = string.Empty;
+
+                // Handle the SelectionChanged event of the ComboBox
+                comboBox.SelectionChanged += (sender, args) =>
+                {
+                    if (args.Source is ComboBox cb)
+                    {
+                        if (cb.SelectedItem != null)
+                        {
+                            selectedItem = cb.SelectedItem.ToString();
+
+                        }
+                    }
+                };
+
+                return selectedItem;
+            }
+            GetSelectedItemString(filter);
+        }
+           
     }
+    
+
+    
 }
