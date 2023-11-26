4ac7e773b34f59cf00bc1977c717e4e43f918882
duclee193 <ducorgan2003@gmail.com>
2023-11-26 23:28:10 +0700

    teampannel.cs

--- Pokemon-Utility/Views/TeamBuilder/TeamPanel.axaml.cs
+++ Pokemon-Utility/Views/TeamBuilder/TeamPanel.axaml.cs
@@ -1,39 +1,16 @@
-/* using System.Collections.Generic;
 using Avalonia.Controls;
-
-namespace Pokemon_Utility.Views.TeamBuilder;
-
-public partial class TeamPanel : Panel
-{
-    // Đức
-    // Ông tạo 1 giao điện hiển thị max 6 con pokemon hoặc có thể it hơn
-    // Trong contructor thứ 2 ông có thể lấy ra các thông tin của pokemon để vẽ giao diện
-    // Ông phải có hiện hình của pokemon, tất cả file hình có trong Assests/pokemon
-    // tên hình là id của con poekmon đó + ".png"
-    // Trang trí sao cho đẹp là đc, nhớ chừa 80 pixel ở bên dưới để người khác thêm các nút chức năng khác
-    private CRUDButton _crudButton = new CRUDButton(2); // mấy cái button này sẽ hiện bên dưới, đừng đụng vào
-    public TeamPanel()
-    {
-        InitializeComponent();
-        this.Children.Add(_crudButton);
-    }
-    public TeamPanel( List<PokemonInfoTeam> pokemonList ) : this()
-    {
-
-    }
-} */
-
-using System;
-using System.Collections.Generic;
-using Avalonia.Controls;
+using Avalonia.Layout;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using System;
+using System.Collections.Generic;
 
 namespace Pokemon_Utility.Views.TeamBuilder
 {
     public partial class TeamPanel : Panel
     {
-        private readonly CRUDButton _crudButton = new CRUDButton(2);
+        private CRUDButton _crudButton = new CRUDButton(2);
+        private List<PokemonInfoTeam> _pokemonList;
 
         public TeamPanel()
         {
@@ -43,34 +20,48 @@ namespace Pokemon_Utility.Views.TeamBuilder
 
         public TeamPanel(List<PokemonInfoTeam> pokemonList) : this()
         {
-            DisplayPokemonList(pokemonList);
+            _pokemonList = pokemonList;
+            InitUI();
         }
 
-        private void DisplayPokemonList(List<PokemonInfoTeam> pokemonList)
+        private void InitUI()
         {
-            const int maxPokemonDisplayed = 6;
-            int displayedPokemonCount = Math.Min(pokemonList.Count, maxPokemonDisplayed);
+            // Xóa các controls cũ nếu có
+            this.Children.Clear();
 
-            for (int i = 0; i < displayedPokemonCount; i++)
+            // Tạo WrapPanel để chứa các Pokemon
+            WrapPanel wrapPanel = new WrapPanel();
+            wrapPanel.Orientation = Orientation.Horizontal;
+
+            foreach (var pokemon in _pokemonList)
             {
-                PokemonInfoTeam pokemon = pokemonList[i];
-
-                // Tạo hình ảnh của Pokémon từ tên file ảnh dựa trên ID của Pokémon
+                // Tạo control hiển thị hình ảnh
+                Image imgPokemon = new Image();
                 Bitmap pokemonImage = new Bitmap(AssetLoader.Open(new Uri($"avares://Pokemon-Utility/Assets/pokemon/{pokemon.Id}.png")));
+                imgPokemon.Source = pokemonImage;
+                imgPokemon.Width = 120;
+                imgPokemon.Height = 120;
 
-                // Hiển thị hình ảnh Pokémon trong một Control (ở đây là Image)
-                Image pokemonImageView = new Image
-                {
-                    Source = pokemonImage,
-                    Width = 100, // Thiết lập kích thước ảnh (có thể điều chỉnh)
-                    Height = 100, // Thiết lập kích thước ảnh (có thể điều chỉnh)
-                    Margin = new Avalonia.Thickness(10) // Khoảng cách giữa các hình ảnh
-                };
+                // Tạo control hiển thị tên Pokemon
+                Label lblPokemonName = new Label();
+                lblPokemonName.Content = pokemon.Name;
+                lblPokemonName.FontSize = 14;
+                lblPokemonName.HorizontalAlignment = HorizontalAlignment.Center;
 
-                // Thêm hình ảnh Pokémon vào TeamPanel
-                this.Children.Add(pokemonImageView);
+                // Thêm các controls vào một StackPanel
+                StackPanel stackPanel = new StackPanel();
+                stackPanel.Children.Add(imgPokemon);
+                stackPanel.Children.Add(lblPokemonName);
+
+                // Thêm StackPanel vào WrapPanel
+                wrapPanel.Children.Add(stackPanel);
             }
+
+            // Thêm WrapPanel vào TeamPanel
+            this.Children.Add(wrapPanel);
+
+            // Chừa 80 pixel ở bên dưới
+            this.Height = this.Children.Count * 120 + 80;
         }
     }
-
 }
